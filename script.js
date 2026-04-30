84ce07ff5333a88ddac915305c297200ec551688
alfarizziibaf <alfarizziibaf@gmail.com>
2026-04-30 10:47:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const toggle = document.getElementById("toggle");
 
 toggle.onclick = () => {
+
 document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+toggle.textContent="☀️";
+}else{
+toggle.textContent="🌙";
+}
+
 };
